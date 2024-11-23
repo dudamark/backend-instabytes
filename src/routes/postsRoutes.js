b0f72998d732fa93fd6d38dc85d8a49f665fcd43
dudamark <dudamark75@gmail.com>
2024-11-23 12:35:21 -0300

    First

---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,44 @@
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import multer from "multer"; // Importa o Multer para lidar com uploads de arquivos
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js"; // Importa as funções controladoras para lidar com a lógica dos posts
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+  
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    
+    cb(null, file.originalname); 
+  }
+});
+
+
+const upload = multer({ storage: storage });
+
+
+const routes = (app) => {
+  
+  app.use(express.json());
+
+  app.use(cors(corsOptions));
+
+ 
+  app.get("/posts", listarPosts); 
+
+  
+  app.post("/posts", postarNovoPost); 
+
+  
+  app.post("/upload", upload.single("imagem"), uploadImagem);
+
+  app.put("/upload/:id", atualizarNovoPost)
+};
+
+export default routes;
